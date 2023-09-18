68dd10277dc4a6f6e757ab37dc3d72f8dbdeca25
Linda <82918531+withLinda@users.noreply.github.com>
2023-09-18 23:50:00 +0700

    Update themes.ts
    Signed-off-by: Linda <82918531+withLinda@users.noreply.github.com>

--- src/styles/theme/themes.ts
+++ src/styles/theme/themes.ts
@@ -2,24 +2,24 @@ const lightTheme = {
   primary: 'rgba(215,113,88,1)',
   text: 'rgba(58,52,51,1)',
   textSecondary: 'rgba(58,52,51,0.7)',
-  background: 'rgba(255,255,255,1)',
+  background: '#fdf6e3',
   backgroundVariant: 'rgba(251,249,249,1)',
   border: 'rgba(58,52,51,0.12)',
   borderLight: 'rgba(58,52,51,0.05)',
-  chatBubbleSystem: 'rgb(59,130,246)',
-  codeBackground: 'rgba(248, 249, 250, 0.95)',
+  chatBubbleSystem: '#2E4C52',
+  codeBackground: '#FFF8ED',
 };
 
 const darkTheme: Theme = {
   primary: 'rgba(220,120,95,1)',
-  text: 'rgba(241,233,231,1)',
-  textSecondary: 'rgba(241,233,231,0.6)',
-  background: '#121212',
-  backgroundVariant: '#1F1B24',
-  border: 'rgba(241,233,231,0.15)',
-  borderLight: 'rgba(241,233,231,0.05)',
-  chatBubbleSystem: 'rgb(41, 41, 41)',
-  codeBackground: 'rgb(20, 21, 23);',
+  text: '#ecc48d',
+  textSecondary: '#7fdbca',
+  background: '#011627',
+  backgroundVariant: '#0b2942',
+  border: '#084d81',
+  borderLight: '#084d81',
+  chatBubbleSystem: '#13344f50',
+  codeBackground: '#0b2942',
 };
 
 export type Theme = typeof lightTheme;
